6829188eedd5037d060cd8229c05154a9c4a45f8
Renata Raffaine Villegas <renataraffaine.gmail.com>
2020-08-23 09:41:12 -0300

    First commit.
    Start Main.cpp logic.
    Next step:
    Implement the CameraCalibration Class and add to main file.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Main class. This class will be used to initialize all the system. 
+
+
+
+int main (int argc,char *argv[])
+{
+	//verify if there's a file on args. If not, ask if the user wants to do a calibration. 
+
+	//Start the camera traking - 1 thread
+	//Start the User interface - 1 thread
+	//Start the communication with Vrep - 1 thread. 
+
+	return 0;
+}
