0efbaf3f049a7b24fa1e97eda70013e5043b91f8
Renata Raffaine Villegas <renataraffaine.gmail.com>
2020-08-24 08:02:23 -0300

    Add source file to camera calibration and learning how to compile it

---
+++ cameraCalibration.cpp
@@ -0,0 +1,29 @@
+#include "cameraCalibration.h"
+using namespace cv;
+using namespace std;
+
+		Mat frame;
+		vector<Mat> AllImages;
+		vector< vector< vector <Point2f> > > allCorners; // vector of all corners detected in all images 
+		vector<vector<int> > allIds;	// all ids of all images 
+		Mat cameraMatrix, distCoeffs; //Camera Params
+		vector <Mat> rvecs, tvecs;  //Used in calibration
+		double repError;	// used in calibration 
+		Size imgSize;	
+		int calibrationFlags; //dont especify any CalibrationFlag 
+		float aspectRatio;   // Used in calibration 
+		int imageCount;
+		String output_path;
+
+	// Constructor 
+		CameraCalibration::CameraCalibration(FileStorage resultFile)
+		{
+
+		}
+		static bool loadCameraCalibration (String resultFile, Mat& cameraMatrix, Mat& distanceCoeff); // Read .txt file
+		bool startCalibration(String path);
+		static bool saveCameraParams(const string &filename, Size imageSize, float aspectRatio, int flags, 
+								 const Mat &cameraMatrix, const Mat &distCoeffs, double totalAvgErr, 
+								 vector <Mat> &rvecs, vector <Mat> &tvecs);
+		void save_images_to_folder(vector<Mat> Images, string ImagesPath);
+
