167821e9912b73ec5359529eac598b8ee9968c75
Renata Raffaine Villegas <renataraffaine.gmail.com>
2020-08-29 11:45:09 -0300

    Add some comments and organize the variables.

--- cameraCalibration.cpp
+++ cameraCalibration.cpp
@@ -2,10 +2,40 @@
 
 using namespace cv;
 using namespace std;
-
-
+	
+	//Webcam frame
+	Mat frame;
+	//Vector of images to calibrate
+	vector<Mat> mAllImages;
+	// vector of all corners detected in all images 
+	vector< vector< vector <Point2f> > > mallCorners;
+	// all ids of all images  
+	vector<vector<int> > mallIds;
+	//Camera Params	
+	Mat cameraMatrix, mdistCoeffs;
+	//Used in calibration 
+	vector <Mat> mrvecs, mtvecs;  
+	// used in calibration 
+	double mrepError =0;	
+	// Image Size
+	Size mimgSize;
+	//dont especify any CalibrationFlag 	
+	int mcalibrationFlags=0; 
+	// Used in calibration 
+	float maspectRatio=0; 
+	// Number of images salved 
+	int mimageCount=0;
+	// Path to resultFile
+	String moutput_path = "";
+	// detectorParameters of the arUco markers
+	Ptr<aruco::DetectorParameters> mdetectorParameters = aruco::DetectorParameters::create();
+	// vectors of points of the makers 
+	vector< vector < Point2f > > mcorners, mrejected;
+	// vector to save the ids of the markers of one image
+	vector <int> mids;
+	Ptr<aruco::Dictionary> mdictionary = aruco::getPredefinedDictionary(aruco::DICT_4X4_50 );
+	Ptr<aruco::CharucoBoard> mcharucoBoard = aruco::CharucoBoard::create(7,7,0.032,0.016,mdictionary);
 	// print some info to the user before calibration. 
-
 	void CameraCalibration::show_info()
 	{
 		cout<<"Welcome to camera calibration.\n"
@@ -19,8 +49,6 @@ using namespace std;
 	{
 		namedWindow ("WebCam", WINDOW_AUTOSIZE);
 		VideoCapture cap(0);
-		Mat frame;
-		Mat image;
 		while (true)
 		{
 		 	cap>>frame;
@@ -37,38 +65,19 @@ using namespace std;
 	
 	bool CameraCalibration::calibrate(String path)
 	{
-	// detect and draw Charuco markers in webcam
-	namedWindow ("WebCam", WINDOW_AUTOSIZE);
-	VideoCapture cap(0);
-	vector<Mat> AllImages;
-	vector< vector< vector <Point2f> > > allCorners; // vector of all corners detected in all images 
-	vector<vector<int> > allIds;	// all ids of all images 
-	Mat cameraMatrix, distCoeffs; //Camera Params
-	vector <Mat> rvecs, tvecs;  //Used in calibration
-	double repError;	// used in calibration 
-	Size imgSize;	
-	int calibrationFlags =0; //dont especify any CalibrationFlag 
-	float aspectRatio =1;   // Used in calibration 
-	cout<< "Press 'c' to capture or q to start calibration\n";
-	Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::DICT_4X4_50 );
-	Ptr<aruco::CharucoBoard> CharucoBoard = aruco::CharucoBoard::create(7,7,0.032,0.016,dictionary);
+
 	}
 
 	Mat CameraCalibration::drawMarkers(Mat frame)
 	{
-		Ptr<aruco::DetectorParameters> mdetectorParameters = aruco::DetectorParameters::create(); 
-		vector< vector < Point2f > > mcorners, mrejected;
-		vector <int> mids;
-		Ptr<aruco::Dictionary> mDictionary = aruco::getPredefinedDictionary(aruco::DICT_4X4_50 );
-		Ptr<aruco::CharucoBoard> mCharucoBoard = aruco::CharucoBoard::create(7,7,0.032,0.016,mDictionary);
 		if (!frame.empty())
 		{
-			aruco::detectMarkers(frame, mDictionary, mcorners, mids, mdetectorParameters, mrejected);
-			aruco::refineDetectedMarkers(frame, mCharucoBoard, mcorners, mids, mrejected);
+			aruco::detectMarkers(frame, mdictionary, mcorners, mids, mdetectorParameters, mrejected);
+			aruco::refineDetectedMarkers(frame, mcharucoBoard, mcorners, mids, mrejected);
 			Mat mcurrentCharucoCorners, mcurrentCharucoIds;
 			if (mids.size()>0)
 			{
-				aruco::interpolateCornersCharuco(mcorners, mids, frame, mCharucoBoard, 
+				aruco::interpolateCornersCharuco(mcorners, mids, frame, mcharucoBoard, 
 												 mcurrentCharucoCorners, mcurrentCharucoIds);
 				aruco::drawDetectedMarkers(frame,mcorners);
 			}
