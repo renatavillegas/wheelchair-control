284eb669e39fb11b187a9431929e88a88ed0038b
Renata Raffaine Villegas <renataraffaine.gmail.com>
2020-11-02 17:33:24 -0300

    Changing object position before start planning.

--- lib/remoteapi.cpp
+++ lib/remoteapi.cpp
@@ -4,12 +4,16 @@ using namespace std;
 using namespace glm;
 
 int clientID=-1;
-int wheelChair;
-int wheelChairIni=-1;
+int robot;
+int robotIni=-1;
+int cameraDummy;
+int cameraDummyIni=-1;
 int arTag;
 int arTagIni=-1;
-int doorPosition; 
-int doorPositionIni = -1;
+int door; 
+int doorIni = -1;
+int goal;
+int goalIni = -1;
 simxFloat TagQuaternion[4];
 simxFloat TagPosition[3];
 Marker realTag;
@@ -28,10 +32,16 @@ void RemoteApi::connect()
 		cout << "ERROR: No connection avaliable\n";
 		simxFinish(clientID);
 	}
-	wheelChairIni = simxGetObjectHandle(clientID, "wheelchair2", &wheelChair, simx_opmode_blocking);
-	if(wheelChairIni==-1)
+	robotIni = simxGetObjectHandle(clientID, "wheelchair2", &robot, simx_opmode_blocking);
+	if(robotIni==-1)
 	{
 		cout << "ERROR: Can't connect to the robot.\n";
+		simxFinish(clientID);
+	}
+	cameraDummyIni = simxGetObjectHandle(clientID, "CameraDummy", &cameraDummy, simx_opmode_blocking);
+	if(cameraDummyIni==-1)
+	{
+		cout << "ERROR: Can't connect to the camera dummy.\n";
 		simxFinish(clientID);
 	}
 	arTagIni = simxGetObjectHandle(clientID, "ARTag", &arTag, simx_opmode_blocking);
@@ -40,10 +50,16 @@ void RemoteApi::connect()
 		cout << "ERROR: Can't find the tag.\n";
 		simxFinish(clientID);
 	}
-	doorPositionIni = simxGetObjectHandle(clientID, "DoorPositionDummy", &doorPosition, simx_opmode_blocking);
-	if(doorPosition == -1)
+	doorIni = simxGetObjectHandle(clientID, "DoorPositionDummy", &door, simx_opmode_blocking);
+	if(doorIni == -1)
 	{
 		cout << "ERROR: Can't find the door.\n";
+		simxFinish(clientID);
+	}
+	goalIni = simxGetObjectHandle(clientID, "GoalCameraDummy", &goal, simx_opmode_blocking);
+	if(goalIni == -1)
+	{
+		cout << "ERROR: Can't find the goal.\n";
 		simxFinish(clientID);
 	}
 }
@@ -59,13 +75,28 @@ void RemoteApi::set_tag_position(Marker tag)
 	simxFloat Position[3] = {(float)realTag.get_position()[0], 
 							 (float)realTag.get_position()[1], 
 							 (float)realTag.get_position()[2]};
-	simxFloat newPos[3];
-	simxGetObjectPosition(clientID, doorPosition, wheelChair, newPos, simx_opmode_blocking);
-	newPos[0]=(float)realTag.get_position()[0];
-	newPos[1]=(float)realTag.get_position()[1];
-	cout << "newpos:" <<newPos[0] <<"; "<< newPos[1] <<"; "<< newPos[2] << endl;
+	simxFloat doorPos[3];
+	simxGetObjectPosition(clientID, door, -1, doorPos, simx_opmode_blocking);
+	simxFloat robotPos[3];
+	simxGetObjectPosition(clientID, cameraDummy, -1, robotPos, simx_opmode_blocking);
+	//change the door position based on the TAG position 
+	doorPos[0]=robotPos[0]+(float)realTag.get_position()[0];
+	doorPos[1]=robotPos[1]+(float)realTag.get_position()[1];
+	cout << "doorPos:" <<doorPos[0] <<"; "<< doorPos[1] <<"; "<< doorPos[2] << endl;
 		realTag.print();
-	simxSetObjectPosition(clientID, doorPosition, wheelChair, newPos, simx_opmode_blocking);
-	//simxSetObjectQuaternion(clientID, doorPosition, robot, RotQuaternion, simx_opmode_blocking);
+
+	simxSetObjectPosition(clientID, door, -1, doorPos, simx_opmode_blocking);
+	//simxSetObjectQuaternion(clientID, doorPosition, wheelChair, RotQuaternion, simx_opmode_blocking);
+	// change the goal position based on the door position.
+	// get the door position in relation to the world 
+	simxGetObjectPosition(clientID, door, -1, doorPos, simx_opmode_blocking);
+	cout << "doorPos2:" <<doorPos[0] <<"; "<< doorPos[1] <<"; "<< doorPos[2] << endl;
+	//get the goalPosition relative to the world
+	simxFloat goalPos[3];
+	simxGetObjectPosition(clientID, goal, -1, goalPos, simx_opmode_blocking);
+	goalPos[0]=doorPos[0];
+	goalPos[1]=doorPos[1]-0.0001;
+	simxSetObjectPosition(clientID, goal, -1, goalPos, simx_opmode_blocking);
+
 }
 
